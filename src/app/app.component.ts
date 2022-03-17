13a07838ca1428d995379b0fd4e113a1f2eb68d4
deepak <deepak.pawar@rsvrtech.com>
2022-03-17 19:18:03 +0530

    for loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+// import { FormControl, FormGroup } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder, NgForm }  from '@angular/forms';
+// import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+   selector: 'app-root',
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.css']
+  
 })
 export class AppComponent {
   title = 'project_name';
+  range = new FormGroup({
+    start: new FormControl(),
+    end: new FormControl(),
+  });
+  count=0
+  counter(typy:string){
+   typy==='add'? this.count++:this.count--;
+  }
+  users =[
+    {name:'deepak',phone:'7692959680'},
+    {name:'sunil',phone:'769295968'},
+    {name:'sumit',phone:'769295680'},
+    {name:'pravin',phone:'769295980'},
+    {name:'kundan',phone:'769259680'},
+    
+  ]
+  userData:any={};
+
+  getData(data:NgForm){
+    console.warn(data)
+    this.userData=data
+  }
+  // toggle element
+  displya=true;
+  toggle()
+  {
+this.displya=!this.displya;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,69 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDialogModule} from '@angular/material/dialog';
+import { DialogComponent } from './dialog/dialog/dialog.component';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatMenuModule} from '@angular/material/menu';
+import { MatSliderModule } from '@angular/material/slider';
+import {CdkAccordionModule} from '@angular/cdk/accordion';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatInputModule} from '@angular/material/input';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatTabsModule} from '@angular/material/tabs';
+    //  matdatepicer
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormControl, FormGroup, FormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogComponent,
+    HeaderComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDialogModule,
+    MatGridListModule,
+    MatMenuModule,
+    MatSliderModule,
+    CdkAccordionModule,
+    DragDropModule,
+    MatSlideToggleModule,
+    MatInputModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatTabsModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatButtonModule,
+    MatNativeDateModule,
+    BrowserAnimationsModule
+    
+    
+    
   ],
-  providers: [],
+  exports:[
+    MatDatepickerModule,
+    MatButtonModule
+  ],
+  providers: [MatFormFieldModule,MatDatepickerModule],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+ 
+ }
